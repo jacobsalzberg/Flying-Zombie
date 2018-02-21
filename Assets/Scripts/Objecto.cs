74a9bd8c5e6efb9648116d9b57147af29b196539
Jacob Salzberg <jacob1818@gmail.com>
2018-02-21 02:16:25 -0300

    Add movable platform script

---
+++ Assets/Scripts/Objecto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objecto : MonoBehaviour {
+
+    [SerializeField] float objectSpeed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.back * (objectSpeed * Time.deltaTime));
+	}
+}
