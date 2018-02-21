2930dd5f2efc88ab468638d49422eefef5281b3f
Jacob Salzberg <jacob1818@gmail.com>
2018-02-21 06:31:25 -0300

    Add full rock move script

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -6,17 +6,37 @@ public class Rock : MonoBehaviour {
 
     [SerializeField] Vector3 topPosition;
     [SerializeField] Vector3 bottomPosition;
+    [Tooltip("How fast does the rock move up and down")]
+    [SerializeField] float speed;
 
     // Use this for initialization
     void Start () {
-		
+        StartCoroutine(Move(bottomPosition));
 	}
 
     IEnumerator Move(Vector3 target)
     {
-        while (Mathf.Abs((target - transform.localPosition).y ) > 0.20f)
+        //target --> where we wanna go
+        //transform.localPosition --> where we currently are
+        // >0.20 --> further out than starting  point
+        while (Mathf.Abs((target - transform.localPosition).y ) >= 0.3f)
         {
+            //If it got to the maximum point, up
+            print ((target-transform.localPosition).y);
+            Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+            transform.localPosition += direction * Time.deltaTime * speed;
 
+            yield return null;
         }
+
+        print("Reached the target");
+
+        yield return new WaitForSeconds(0.5f);
+
+        //check if its at the top or the bottom.. if it's at top, target = bottom, else target=top
+        Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+        //Call itself over again!!
+        StartCoroutine(Move(newTarget)); 
     }
-}
+}                                    
