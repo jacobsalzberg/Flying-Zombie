97e7d29dcec5511d240be4cca37574db58327741
Jacob Salzberg <jacob1818@gmail.com>
2018-02-21 04:41:28 -0300

    Add Rock

--- Assets/Scripts/Objecto.cs
+++ Assets/Scripts/Objecto.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Objecto : MonoBehaviour {
 
-    [SerializeField] float objectSpeed = 1;
-	// Use this for initialization
-	void Start () {
+    [SerializeField] private float objectSpeed = 1;
+    private float resetPosition = -14.58f;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.back * (objectSpeed * Time.deltaTime));
+
+        if (transform.localPosition.z <= resetPosition)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, 49.84f);
+            transform.position = newPos;
+        }
+
 	}
 }
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    IEnumerator Move(Vector3 target)
+    {
+        while (Mathf.Abs((target - transform.localPosition).y ) > 0.20f)
+        {
+
+        }
+    }
+}
